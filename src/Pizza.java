fa7a8c317b3fd280d59c417d4c1bd5fccc8cce4d
Mbroholm <mibr0005@stud.kea.dk>
2025-03-20 16:59:01 +0100

    intellij config + minor fixes

--- src/Pizza.java
+++ src/Pizza.java
@@ -2,7 +2,7 @@ public class Pizza {
     private final String name;
     private double price;
 
-    public Pizza(int id, String name, double price) {
+    public Pizza(String name, double price) {
         this.name = name;
         this.price = price;
     }
@@ -18,5 +18,7 @@ public class Pizza {
     public void setPrice(double price) { this.price = price; }
 
     @Override
-    public String toString() { return "Pizza: " +  name  + "Price: " + price; }
+    public String toString() {
+        return name  + ", " + price + " kr.";
+    }
 }
